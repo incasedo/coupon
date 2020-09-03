51f78f374ccd589fe5ecca84f577b7e14e7452bf
pang453758463 <383354826@qq.com>
2020-09-03 10:40:22 +0800

    优惠券预发布

---
+++ coupon.sql
@@ -0,0 +1,6 @@
+INSERT INTO `auth_rules` (`id`, `api`, `url`, `icon`, `title`, `pid`, `state`, `sort`) VALUES
+(null, 'Coupon', '', '', '优惠券', 23, 1, 0),
+(null, 'CouponList', '', '', '优惠券列表', 161, 1, 0),
+(null, 'CreateCoupon', '', '', '添加优惠券', 161, 0, 0),
+(null, 'EditCoupon', '', '', '优惠券操作', 161, 0, 0),
+(null, 'DeleteCoupon', '', '', '优惠券删除', 161, 0, 0);
